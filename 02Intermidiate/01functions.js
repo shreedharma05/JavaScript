d727c9cbac3ce554d6986aa95500c0589a992a46
Shreedharma <105602555+shreedharma05@users.noreply.github.com>
2022-10-13 23:23:28 +0530

    Create 01functions.js

---
+++ 02Intermidiate/01functions.js
@@ -0,0 +1,16 @@
+function sayHello(name){
+    console.log('Hey, there Shree dharma')
+    console.log('Hey, there ' + name)
+    console.log(`Hey, there ${name} Hope you are doing good`)
+}
+
+sayHello()
+sayHello('Romeo')
+
+function namaste(){
+    return 'greeting in India'
+}
+
+greet = namaste()
+console.log(greet)
+console.log(namaste())
