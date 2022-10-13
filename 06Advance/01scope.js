a050aa93ad280ac94c85a1219f7b7f73c54c2896
Shreedharma <105602555+shreedharma05@users.noreply.github.com>
2022-10-13 23:35:41 +0530

    Create 01scope.js

---
+++ 06Advance/01scope.js
@@ -0,0 +1,14 @@
+// console.log(name)
+// var name = "Shree dharma"
+
+// using var keyword
+if(true){
+    var firstName = "Shree dharma"
+}
+console.log(firstName)
+
+// using let keyword
+if (true){
+    let lastName = "Kr"
+}
+console.log(lastName)
