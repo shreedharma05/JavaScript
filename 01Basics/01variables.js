ed4253dbc33461ca0255e09580c68e6aeafa0f59
Shreedharma <105602555+shreedharma05@users.noreply.github.com>
2022-10-13 23:21:13 +0530

    Create 01variables.js

---
+++ 01Basics/01variables.js
@@ -0,0 +1,20 @@
+var full_name = 'Shree dharma';
+
+//How to name variables
+
+// fullname - Camel case
+// full-name
+// full_name - Snake case
+// FullName - Avoid
+
+var courseName = 'js';
+var isLoggedIn = false;
+var loggedCount = 45;
+
+console.log(loggedCount);
+console.log('loggedCount');
+
+var paymentMode;
+
+paymentMode = 'Credit Card'
+console.log(paymentMode);
